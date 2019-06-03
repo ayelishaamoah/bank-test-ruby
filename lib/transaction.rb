938de75b0384253c008f6793720ffc3f9d8e6a1e
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-06-03 16:28:21 +0100

    Add current date to transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,9 +2,10 @@
 
 # Instances of Transaction class will be stored in the main account class
 class Transaction
-  attr_reader :amount
+  attr_reader :amount, :date
 
   def initialize(amount)
     @amount = amount
+    @date = Time.now
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,8 +3,15 @@
 require 'transaction'
 
 describe Transaction do
-  it 'it stores the transaction amount' do
+  it 'stores the transaction amount' do
     transaction = Transaction.new(200)
     expect(transaction.amount).to eq 200
+  end
+
+  it 'stores the transaction date' do
+    transaction = Transaction.new(200)
+    date_now = Time.now
+    allow(transaction).to receive(:date).and_return(date_now)
+    expect(transaction.date).to eq date_now
   end
 end
