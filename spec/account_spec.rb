a43c1f92471dadabc5ec369ce508dd2acda07d18
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-06-03 15:03:16 +0100

    Refactored tests to remove repitition

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,17 +2,19 @@
 
 require 'account'
 
-describe 'show balance' do
-  it 'initialised with a balance of zero' do
-    account = Account.new
-    expect(account.balance).to eq 0
-  end
-end
+describe Account do
+  subject { Account.new }
 
-describe 'deposit' do
-  it 'increased the balance by 100' do
-    account = Account.new
-    account.deposit(100)
-    expect(account.balance).to eq 100
+  describe 'show balance' do
+    it 'initialised with a balance of zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe 'deposit' do
+    it 'increased the balance by 100' do
+      subject.deposit(100)
+      expect(subject.balance).to eq 100
+    end
   end
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 require 'account'
+require 'spec_helper'
 
-feature 'show balance' do
-  scenario 'creating a new account with a balance of zero' do
-    account = Account.new
-    expect(account.show_balance).to eq 0
+describe 'Account' do
+  subject { Account.new }
+
+  feature 'show balance' do
+    scenario 'creating a new account with a balance of zero' do
+      expect(subject.show_balance).to eq 0
+    end
   end
-end
 
-# a user can deposit money into an account
-feature 'create a new transaction' do
-  scenario 'can deposit money into an account' do
-    account = Account.new
-    account.deposit(100)
-    expect(account.show_balance).to eq 100
+  # a user can deposit money into an account
+  feature 'create a new transaction' do
+    scenario 'can deposit money into an account' do
+      subject.deposit(100)
+      expect(subject.show_balance).to eq 100
+    end
   end
 end
